f2ebe87aded1cd25b245a2a1bb5f62966bd0abae
Aditya J <ajain15@uic.edu>
2018-08-16 17:12:26 -0400

    remove key error

--- src/components/ThreeSankey.vue
+++ src/components/ThreeSankey.vue
@@ -3,7 +3,7 @@
     <canvas id="drawingBoard" :width="correctedWidth" :height="correctedHeight"/>
     <template v-for="(node, i) in this.textNodes">
       <span :key="i" :style=getSpanPosition(node)>{{node.name}}</span>
-      <div class="sankeyNodes" :key="i" :style=getNodePosition(node)></div>
+      <div class="sankeyNodes" :key="node.name" :style=getNodePosition(node)></div>
     </template>
   </div>
 </template>
